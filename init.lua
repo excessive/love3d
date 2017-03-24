7ca66fe1903f4c0a71b34d86550458b24235c898
Colby Klein <shakesoda@gmail.com>
2017-03-24 09:08:29 -0700

    remove terrible monkeypatch api, it made things worse than it did better.

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ local use_gles = false
 local l3d = {
 	_LICENSE = "Love3D is distributed under the terms of the MIT license. See LICENSE.md.",
 	_URL = "https://github.com/excessive/love3d",
-	_VERSION = "0.0.1",
+	_VERSION = "0.0.2",
 	_DESCRIPTION = "A 3D extension for LÖVE."
 }
 
@@ -53,8 +53,7 @@ end
 --
 -- This must be called before anything else.
 -- @param use_monkeypatching patch the LOVE API with LOVE3D functions
--- @param automatic_transforms attempt to automatically upload transformation matrices
-function l3d.import(use_monkeypatching, automatic_transforms)
+function l3d.import()
 	local already_loaded = pcall(function() return ffi.C.SDL_GL_DEPTH_SIZE end)
 	if not already_loaded then
 		ffi.cdef([[
@@ -92,19 +91,8 @@ function l3d.import(use_monkeypatching, automatic_transforms)
 	end
 	opengl:import()
 
-	l3d._state = {}
-	l3d._state.stack = {}
-	l3d.push("all")
-
 	local out = ffi.new("int[?]", 1)
 	sdl.SDL_GL_GetAttribute(sdl.SDL_GL_DEPTH_SIZE, out)
-
-	assert(out[0] > 8, "We didn't get a depth buffer, bad things will happen.")
-	print(string.format("Depth bits: %d", out[0]))
-
-	if use_monkeypatching then
-		l3d.patch(automatic_transforms == nil and true or automatic_transforms)
-	end
 end
 
 --- Clear color/depth buffers.
@@ -133,6 +121,7 @@ function l3d.reset()
 end
 
 -- FXAA helpers
+--- Get FXAA alpha value for a given color
 function l3d.get_fxaa_alpha(color)
 	local c_vec = cpml.vec3.isvector(color) and color or cpml.vec3(color)
 	return c_vec:dot(cpml.vec3(0.299, 0.587, 0.114))
@@ -150,7 +139,10 @@ function l3d.set_depth_write(mask)
 	if mask then
 		assert(type(mask) == "boolean", "set_depth_write expects one parameter of type 'boolean'")
 	end
-	gl.DepthMask(mask or true)
+	if mask == nil then
+		mask = true
+	end
+	gl.DepthMask(mask and 1 or 0)
 end
 
 --- Set depth test method.
@@ -272,7 +264,7 @@ function l3d.new_shader_raw(gl_version, vc, pc)
 #line 0
 %s]]
 		local vs = arg1 and string.format(fmt, "#version " .. versions[gl_version], "VERTEX", vc) or nil
-		local ps = arg2 and string.format(fmt, versions[gl_version], "PIXEL", pc) or nil
+		local ps = arg2 and string.format(fmt, "#version " .. versions[gl_version], "PIXEL", pc) or nil
 		return vs, ps
 	end
 	local orig = love.graphics._shaderCodeToGLSL
@@ -280,124 +272,6 @@ function l3d.new_shader_raw(gl_version, vc, pc)
 	local shader = love.graphics.newShader(vc, pc)
 	love.graphics._shaderCodeToGLSL = orig
 	return shader
-end
-
---- Update the active shader.
--- Used internally by patched API, update it yourself otherwise.
--- This is important for l3d.push/pop and update_matrix.
--- @param shader
-function l3d.update_shader(shader)
-	l3d._active_shader = shader
-end
-
---- Push the current matrix to the stack.
-function l3d.push(which)
-	local stack = l3d._state.stack
-	assert(#stack < 64, "Stack overflow - your stack is too deep, did you forget to pop?")
-	if #stack == 0 then
-		table.insert(stack, {
-			projection = false,
-			matrix = cpml.mat4(),
-			active_shader = l3d._active_shader,
-		})
-	else
-		-- storing the active shader is useful, but don't touch it!
-		local top = stack[#stack]
-		local new = {
-			projection = top.projection,
-			matrix = top.matrix:clone(),
-			active_shader = top.active_shader,
-		}
-		if which == "all" then
-			-- XXX: I hope this is what's expected.
-			new.active_shader = top.active_shader
-		end
-		table.insert(stack, new)
-	end
-	l3d._state.stack_top = stack[#stack]
-end
-
---- Pop the current matrix off the stack.
-function l3d.pop()
-	local stack = l3d._state.stack
-	assert(#stack > 1, "Stack underflow - you've popped more than you pushed!")
-	table.remove(stack)
-
-	local top = stack[#stack]
-	l3d._state.stack_top = top
-end
-
---- Translate the current matrix.
--- @param x vec3 or x translation
--- @param y y translation
--- @param z z translation (or 1)
-function l3d.translate(x, y, z)
-	local top = l3d._state.stack_top
-	top.matrix = top.matrix:translate(cpml.vec3(x, y, z or 0))
-end
-
---- Translate the current matrix.
--- @param r rotation angle in radians
--- @param axis axis to rotate about. Z if unspecified.
-function l3d.rotate(r, axis)
-	local top = l3d._state.stack_top
-	if type(r) == "table" and r.w then
-		top.matrix = top.matrix:rotate(r)
-		return
-	end
-	assert(type(r) == "number")
-	top.matrix = top.matrix:rotate(r, axis or cpml.vec3.unit_z)
-end
-
---- Scale the current matrix.
--- @param x vec3 or x scale
--- @param y y scale
--- @param z z scale (or 1)
-function l3d.scale(x, y, z)
-	local top = l3d._state.stack_top
-	top.matrix = top.matrix:scale(cpml.vec3(x, y, z or 1))
-end
-
---- Reset the current matrix.
-function l3d.origin()
-	local top = l3d._state.stack_top
-	top.matrix = top.matrix:identity()
-end
-
---- Return the current matrix.
--- @return mat4
-function l3d.get_matrix()
-	return l3d._state.stack_top.matrix
-end
-
---- Send matrix to the active shader.
--- Convenience function.
---
--- Valid matrix types are currently "transform" and "projection".
--- A "view" type will likely be added in the future for convenience.
--- @param matrix_type
--- @param m
-function l3d.update_matrix(matrix_type, m)
-	if --[[use_gles and]] not l3d._active_shader then
-		return
-	end
-	local w, h
-	if matrix_type == "projection" then
-		w, h = love.graphics.getDimensions()
-	end
-	local send_m = m or matrix_type == "projection" and cpml.mat4():ortho(0, w, 0, h, -100, 100) or l3d.get_matrix()
-	-- XXX: COMPLETE HORSE SHIT. Love uses GL1.0 matrix loading for performance.
-	-- if not use_gles then
-		-- local buf = ffi.new("GLfloat[?]", 16, send_m)
-		-- gl.MatrixMode(matrix_type == "transform" and GL.MODELVIEW or GL.PROJECTION)
-		-- gl.LoadMatrixf(buf)
-	-- else
-		-- ...But on ES it uses glUniformMatrix like it should.
-		l3d._active_shader:send(matrix_type == "transform" and "u_model" or "u_projection", send_m:to_vec4s())
-	-- end
-	if matrix_type == "projection" then
-		l3d._state.stack_top.projection = true
-	end
 end
 
 --- Create a buffer from a list of vertices (cpml.vec3's).
@@ -447,6 +321,7 @@ end
 function l3d.new_canvas(width, height, format, msaa, gen_depth)
 	-- TODO: Test this to make sure things are properly freed.
 	if use_gles then
+		error()
 		return
 	end
 	local w, h = width or love.graphics.getWidth(), height or love.graphics.getHeight()
@@ -484,6 +359,7 @@ function l3d.bind_shadow_map(map)
 	if map then
 		assert(map.shadow_map)
 		gl.DrawBuffer(GL.NONE)
+		gl.ReadBuffer(GL.NONE)
 		love.graphics.setCanvas(map.dummy_canvas)
 		gl.BindFramebuffer(GL.FRAMEBUFFER, map.buffers[0])
 		gl.Viewport(0, 0, map.width, map.height)
@@ -492,6 +368,7 @@ function l3d.bind_shadow_map(map)
 		-- gl.BindFramebuffer(0)
 		love.graphics.setCanvas()
 		gl.DrawBuffer(GL.BACK)
+		gl.ReadBuffer(GL.BACK)
 	end
 end
 
@@ -557,144 +434,6 @@ function l3d.new_shadow_map(w, h)
 		width        = w,
 		height       = h
 	}
-end
-
---[[
--- depth-only canvas!
-local function l3d.new_depth_canvas(w, h)
-	local fbo = ffi.new("unsigned int[1]", 1)
-	gl.GenFramebuffers(1, fbo)
-	gl.BindFramebuffer(GL.FRAMEBUFFER, fbo[0])
-
-	local depth = ffi.new("unsigned int[1]", 1)
-	gl.GenTextures(1, depth)
-	gl.BindTexture(GL.TEXTURE_2D, depth[0])
-	gl.TexImage2D(GL.TEXTURE_2D, 0, GL.DEPTH_COMPONENT24, w, h, 0, GL.DEPTH_COMPONENT, GL.FLOAT, 0)
-	gl.TexParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST)
-	gl.TexParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST)
-	gl.TexParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE)
-	gl.TexParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE)
-
-	gl.FramebufferTexture(GL.FRAMEBUFFER, GL.DEPTH_ATTACHMENT, depth, 0)
-
-	gl.DrawBuffer(GL.NONE) -- No color buffer is drawn to.
-
-	if gl.CheckFramebufferStatus(GL.FRAMEBUFFER) ~= GL.FRAMEBUFFER_COMPLETE then
-		return false
-	end
-
-	return fbo
-end
---]]
-
---- Patch functions into the LOVE API.
--- Automatically called by l3d.import(true).
---
--- This isn't good practice (which is why you must explicitly call it), but
--- patching various love functions to maintain state here makes things a lot
--- more pleasant to use.
---
--- Do not call this function if you've already called import(true).
--- @param automatic_transforms
-function l3d.patch(automatic_transforms)
-	--- Get a handle to the library.
-	love.graphics.getLove3D     = function() return l3d end
-	--- See l3d.clear.
-	love.graphics.clearDepth    = function() l3d.clear() end
-	--- See l3d.set_depth_test.
-	-- @function love.graphics.setDepthTest
-	love.graphics.setDepthTest  = l3d.set_depth_test
-	--- See l3d.set_depth_write.
-	-- @function love.graphics.setDepthWrite
-	love.graphics.setDepthWrite = l3d.set_depth_write
-	--- See l3d.set_culling.
-	-- @function love.graphics.setCulling
-	love.graphics.setCulling    = l3d.set_culling
-	--- See l3d.set_front_face.
-	-- @function love.graphics.setFrontFace
-	love.graphics.setFrontFace  = l3d.set_front_face
-	--- See l3d.reset.
-	-- @function love.graphics.reset
-	love.graphics.reset         = combine(l3d.reset, love.graphics.reset)
-
-	-- XXX: RE: Automatic transforms.
-	-- You should basically only do this if you are Karai and have no idea how
-	-- the fuck to operate a GPU. This will probably make your code slow and
-	-- cause horrible things to happen to you, your family and your free time.
-	local update = automatic_transforms and function() l3d.update_matrix("transform") end or function() end
-
-	local reset_proj = function()
-		if not automatic_transforms then
-			return
-		end
-		local stack = l3d._state.stack
-		local below = stack[#stack-1]
-		if (below and not below.projection) or not below then
-			l3d.update_matrix("projection")
-		end
-	end
-
-	--- See l3d.origin.
-	-- @function love.graphics.origin
-	love.graphics.origin        = combine(l3d.origin, love.graphics.origin, update)
-	--- See l3d.pop.
-	-- @function love.graphics.pop
-	love.graphics.pop           = combine(l3d.pop, love.graphics.pop, update, reset_proj)
-	--- See l3d.push.
-	-- @function love.graphics.push
-	love.graphics.push          = combine(l3d.push, love.graphics.push)
-	-- no use calling love's function if it will explode.
-	local orig = {
-		translate = love.graphics.translate,
-		rotate    = love.graphics.rotate,
-		scale     = love.graphics.scale
-	}
-	--- See l3d.rotate.
-	love.graphics.rotate = function(r, axis)
-		if type(r) == "number" then
-			orig.rotate(r)
-		end
-		l3d.rotate(r, axis or cpml.vec3.unit_z)
-		update()
-	end
-	--- See l3d.scale.
-	love.graphics.scale = function(x, y, z)
-		if type(x) == "table" and cpml.vec3.isvector(x) then
-			l3d.scale(x)
-			update()
-			return
-		end
-		l3d.scale(x, y, z)
-		orig.scale(x, y)
-		update()
-	end
-	--- See l3d.translate.
-	love.graphics.translate = function(x, y, z)
-		if type(x) == "table" and cpml.vec3.isvector(x) then
-			l3d.translate(x)
-			update()
-			return
-		end
-		l3d.translate(x, y, z)
-		orig.translate(x, y)
-		update()
-	end
-	--- See l3d.get_matrix.
-	-- @function love.graphics.getMatrix
-	love.graphics.getMatrix     = l3d.get_matrix
-	--- See l3d.update_matrix.
-	-- @function love.graphics.updateMatrix
-	-- @param matrix_type
-	-- @param m
-	love.graphics.updateMatrix  = l3d.update_matrix
-
-	--- See l3d.update_shader.
-	-- @function love.graphics.setShader
-	-- @param shader
-	love.graphics.setShader     = combine(l3d.update_shader, love.graphics.setShader, update)
-	--- See l3d.new_canvas.
-	-- @function love.graphics.newCanvas
-	love.graphics.newCanvas     = l3d.new_canvas
 end
 
 return l3d
